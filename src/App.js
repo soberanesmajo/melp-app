513587d3e2aca4558fe38dec24f9ef71e6798995
soberanesmajo <soberanesmajo@gmail.com>
2019-02-25 16:23:32 -0600

    agrega componentes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import Header from './Components/Header/Header';
+import Cards from './Components/Cards/Cards';
+//import Data from './data_melp.json';
 import './App.css';
 
 class App extends Component {
+
+  // gettingData = (Data) =>{
+  //   console.log(Data);
+    
+  // }
+
+  
   render() {
+    //this.gettingData(Data)
     return (
       <div className="App">
-        <header className="App-header">
-        </header>
+        <Header />
+        <Cards />
       </div>
     );
   }
