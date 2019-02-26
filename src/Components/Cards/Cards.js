601cd39f92f4f7b65cd37fdd42e4a85a1af55144
soberanesmajo <soberanesmajo@gmail.com>
2019-02-25 20:48:42 -0600

    agrega estado

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -2,32 +2,58 @@ import React, {Component} from 'react';
 import Data from '../../data_melp.json';
 
 class Cards extends Component {
-    gettingData(Data) {
-        //console.log(Data); 
-        const info = Data.forEach(element => {
-            let name = element.name;
-            //console.log(name);    
-            let rating = element.rating;
-            //console.log(rating);
-            let contact = element.contact;
-            //console.log(contact);
-            let address = element.address;
-            console.log(address);                    
-        });
+    constructor() {
+        super();
+        this.state = {}
     }
     
     componentDidMount () {
-        this.gettingData(Data);
+        const info = Data.forEach(element => {
+            let name = element.name;
+            console.log(name);    
+            let rating = element.rating;
+            let site = element.contact.site;
+            let phone = element.contact.phone; 
+            let street = element.address.street;
+            //console.log(street);
+            let city = element.address.city;
+            let state= element.address.state;
+            let dataCard = {
+                name: name,
+                rating: rating,
+                site: site,
+                phone: phone,
+                street: street,
+                city: city,
+                state: state
+            }
+            this.setState({dataCard:[...this.state.dataCard, dataCard]});
+        });
     }
-
+    
     render () {
         return (
             <p>Holi</p>
-        )
+            )
+        }       
     }
+    
+    export default Cards;
 
-}
 
 
-export default Cards;
+        // gettingData(Data) {
+        //     //console.log(Data); 
+        //     const info = Data.forEach(element => {
+        //         let name = element.name;
+        //         //console.log(name);    
+        //         let rating = element.rating;
+        //         let site = element.contact.site;
+        //         let phone = element.contact.phone; 
+        //         let street = element.address.street;
+        //         //console.log(street);
+        //         let city = element.address.city;
+        //         let state= element.address.state;
+        //     });
+        // }
 
