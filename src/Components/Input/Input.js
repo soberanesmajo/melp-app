c62533b0d725445e980d852c22c52a7edb708182
soberanesmajo <soberanesmajo@gmail.com>
2019-02-26 21:10:01 -0600

    agrega algunos estilos

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Input = () => (
-    <div class="input-group mb-3">
-        <select class="custom-select" id="inputGroupSelect01">
-            <option selected>Filtrar por...</option>
-            <option value="1">Raiting</option>
-            <option value="2">Orden alfabético</option>
-        </select>
-    </div>
-)
+class Input extends Component {
+    render (){
+        //console.log(this.props);
+        return(
+            <div className="input-group mb-3">
+                <select className="custom-select" id="inputGroupSelect01">
+                    <option selected>Filtrar por...</option>
+                    <option value="1">Raiting</option>
+                    <option value="2">Orden alfabético</option>
+                </select>
+            </div>
+        )
+    }
+}
 
 export default Input;
