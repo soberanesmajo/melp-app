f277f10c78bd4c8519ab767af4d2646841546ec2
soberanesmajo <soberanesmajo@gmail.com>
2019-02-27 00:13:20 -0600

    Updates

---
+++ precache-manifest.22df6d3e464047ef90af5680e298866f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5154d7d303385a00b3f",
+    "url": "/melp-app/static/js/runtime~main.b5154d7d.js"
+  },
+  {
+    "revision": "3fe6982580903daf9415",
+    "url": "/melp-app/static/js/main.3fe69825.chunk.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/melp-app/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "3fe6982580903daf9415",
+    "url": "/melp-app/static/css/main.49181956.chunk.css"
+  },
+  {
+    "revision": "9c2d1aff3b8a51da3aa9a2602438399b",
+    "url": "/melp-app/index.html"
+  }
+];
